45786c9496949c55e6568d8b974ea778669ef54c
Ivan Cavero <ivangonzalezcavero@gmail.com>
2024-03-10 00:28:27 +0100

    🔨 Add vercel deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "hybrid",
+  adapter: vercel()
 });
