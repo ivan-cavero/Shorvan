5a4a9dec4c914507b87b8d5ba67a63d7cb55283a
Ivan Cavero <ivangonzalezcavero@gmail.com>
2024-04-14 18:51:11 +0200

    Update Astro configuration and Playwright settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,11 +23,9 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    baseURL: 'http://localhost:4321/',
   },
 
   /* Configure projects for major browsers */
@@ -68,10 +66,10 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'bun start',
+    url: 'http://localhost:4321/',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  }
 });
